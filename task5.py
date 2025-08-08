1197cedd920a11402814c738a44f484887f2ea2e
FILIBUS YILRIT DIMKA <110455012+dimka90@users.noreply.github.com>
2025-08-08 11:53:15 +0100

    Create task5.py
    add task5.py

---
+++ task5.py
@@ -0,0 +1,14 @@
+"""
+Task 5: Grading System
+
+Write a Python program that asks the user for a student’s score (0–100)
+and prints:
+- A if it is 70–100
+- B if 60–69
+- C if 50–59
+- D if 45–49
+- E if 40–44
+- F if 0–39
+
+If the score is outside this range, print "Invalid score".
+"""
