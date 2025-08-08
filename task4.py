4b6155440c891213c1884c81afadd2b7e4a29340
FILIBUS YILRIT DIMKA <110455012+dimka90@users.noreply.github.com>
2025-08-08 11:49:34 +0100

    Create task4.py
    Task: add task4.py

---
+++ task4.py
@@ -0,0 +1,11 @@
+"""
+Task 4: Dictionary Update
+
+Create a key-value pair variable `students` and add one student record.
+A student should have:
+- name
+- level
+- score
+
+Use any method to update the student's score by +10.
+"""
